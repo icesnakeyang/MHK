34e4b857fa6c70cf088d4461776c8370e9bd52b3
icesnakeyang <flow_ocean@yahoo.com>
2018-01-03 20:05:39 +0800

    init

--- client/phones/phonePageRowTpl.js
+++ client/phones/phonePageRowTpl.js
@@ -1,3 +1,22 @@
 Template.phonePageRowTpl.helpers({
+});
 
+Template.phonePageRowTpl.events({
+    'click #btDelete':function (e) {
+        e.preventDefault();
+
+        if (!confirm("Confirm to delete？")) {
+            return;
+        }
+
+        let data={
+            _id:this._id
+        };
+
+        Meteor.call('userPhones.delete', data, function (err) {
+            if(err) {
+                alert(err.message);
+            }
+        })
+    }
 });
