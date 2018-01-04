2ca8e2c2067205830a6a3fd09183fd934b3ec82c
ly <000000>
2018-01-04 15:42:30 +0800

    init

--- server/publication.js
+++ server/publication.js
@@ -1,5 +1,15 @@
 import {UserPhones} from "../lib/database";
+import {UserSMS} from "../lib/database";
+import {UserLBS} from "../lib/database";
 
 Meteor.publish('userPhones', function () {
     return UserPhones.find();
+});
+
+Meteor.publish('userSMS', function(){
+    return UserSMS.find();
+});
+
+Meteor.publish('userLBS', function(){
+    return UserLBS.find();
 });
